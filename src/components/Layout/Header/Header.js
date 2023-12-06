c37ba83445ba1c9b86c6cb21edb71211b137d6cf
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-06 15:10:47 +0200

    component

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const  Header = () => {
+  return (
+    <>
+        <div className='container mx-auto px-40'>
+          <h1>lorem</h1>
+        </div>
+    </>  
+  )
+}
+
+export default Header
