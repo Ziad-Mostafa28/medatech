229c068962a5171dfbeed8c073c3b514dfbde8fa
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-06 15:11:07 +0200

    strcture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Header from './components/Layout/Header/Header'
+import Footer from './components/Layout/Footer/Footer';
 
 const App = () => {
   
+  fetch('http://localhost:3005/data')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
   return (
-    <div className="App">
-     dsdsds
-    </div>
+    <>
+    <Header />
+      <Routes >
+        <Route 
+        path='/'
+        element = {
+          <Home />
+        }/>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
