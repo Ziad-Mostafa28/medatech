a36494891bc149ffc567d9dce01d161c7ba40ff4
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-11 14:00:50 +0200

    add component

---
+++ src/components/Sliders/Categories.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Navigation, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+
+const Categories = () => {
+const [Categories, setCategories] = useState([]);
+
+fetch("http://localhost:3005/Brands")
+    .then((response) => response.json())
+    .then((data) => setCategories(data));
+
+return (
+    <div className="container mx-auto w-4/5 mt-6">
+    <p className="text-3xl text-[#253D4E] font-[Quicksand] font-bold">
+        Categories
+    </p>
+    <Swiper
+        modules={[Navigation, A11y]}
+        spaceBetween={2}
+        slidesPerView={10}
+        breakpoints={{
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 7,
+            spaceBetween: 2,
+        },
+        1440: {
+            slidesPerView: 10,
+            spaceBetween: 2,
+        },
+        }}
+        loop={true}
+        navigation
+        className="mySwiper"
+    >
+        {Categories &&
+        Categories?.map((data) => (
+            <SwiperSlide className="text-center">
+            <div
+                key={data?.id}
+                value={data?.id}
+                className={`h-[160px] text-center inline-block rounded-2xl	 bg-[${data?.color}]`}
+            >
+                <img
+                className="w-[100px] h-[108px]"
+                src={data.img}
+                alt={data?.name}
+                />
+                <p className="mt-2 text-m text-[#253D4E] font-[Quicksand] font-bold">
+                {data?.text}
+                </p>
+            </div>
+            </SwiperSlide>
+        ))}
+    </Swiper>
+    </div>
+);
+};
+
+export default Categories;
+
+// {`logo_${data?.id}.png`}
