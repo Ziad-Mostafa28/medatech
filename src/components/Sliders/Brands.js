1da5e0f7e9d26d9cc90b3f90a641886e6d4fc8f7
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-11 14:00:35 +0200

    update

--- src/components/Sliders/Brands.js
+++ src/components/Sliders/Brands.js
@@ -6,69 +6,69 @@ import "swiper/css/navigation";
 import "swiper/css/scrollbar";
 
 const Brands = () => {
-  const [Brands, setBrands] = useState([]);
+const [Brands, setBrands] = useState([]);
 
-  fetch("http://localhost:3005/Brands")
+fetch("http://localhost:3005/Brands")
     .then((response) => response.json())
     .then((data) => setBrands(data));
 
-  return (
-    <div className="container mx-auto w-4/5 mt-6 mb-12">
-      <p className="mb-6 text-3xl text-[#253D4E] font-[Quicksand] font-bold">
+return (
+    <div className="container mx-auto w-4/5 mt-6">
+    <p className="text-3xl text-[#253D4E] font-[Quicksand] font-bold">
         Brands
-      </p>
-      <Swiper
+    </p>
+    <Swiper
         modules={[Navigation, A11y]}
         spaceBetween={2}
         slidesPerView={10}
         breakpoints={{
-          320: {
+        320: {
             slidesPerView: 2,
             spaceBetween: 15,
-          },
-          640: {
+        },
+        640: {
             slidesPerView: 3,
             spaceBetween: 20,
-          },
-          768: {
+        },
+        768: {
             slidesPerView: 5,
             spaceBetween: 20,
-          },
-          1024: {
+        },
+        1024: {
             slidesPerView: 7,
             spaceBetween: 2,
-          },
-          1440: {
+        },
+        1440: {
             slidesPerView: 10,
             spaceBetween: 2,
-          },
+        },
         }}
         loop={true}
         navigation
         className="mySwiper"
-      >
+    >
         {Brands &&
-          Brands?.map((data) => (
+        Brands?.map((data) => (
             <SwiperSlide className="text-center">
-              <div
+            <div
                 key={data?.id}
                 value={data?.id}
                 className={`h-[160px] text-center inline-block rounded-2xl	 bg-[${data?.color}]`}
-              >
+            >
                 <img
-                  className="w-[100px] h-[108px]"
-                  src={data.img}
-                  alt={data?.name}
+                className="w-[100px] h-[108px]"
+                src={data.img}
+                alt={data?.name}
                 />
                 <p className="mt-2 text-m text-[#253D4E] font-[Quicksand] font-bold">
-                  {data?.text}
+                {data?.text}
                 </p>
-              </div>
+            </div>
             </SwiperSlide>
-          ))}
-      </Swiper>
+        ))}
+    </Swiper>
     </div>
-  );
+);
 };
 
 export default Brands;
