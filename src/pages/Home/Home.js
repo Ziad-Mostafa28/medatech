fef93d49fd267db3ed7e830f48ebd403b5267c56
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-11 14:01:11 +0200

    add component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
 import Brands from '../../components/Sliders/Brands'
+import Categories from '../../components/Sliders/Categories'
+import ProductCard from '../../components/Sliders/Popular'
+
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Brands />
+      <Categories />
+      <ProductCard/>
     </>
   )
 }
