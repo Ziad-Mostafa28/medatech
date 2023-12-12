c1a89ae0033071f9e3f5c77bce645cab6fb2481d
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-12 16:43:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home/Home';
-import Header from './components/Layout/Header/Header'
-import Footer from './components/Layout/Footer/Footer';
+import Home from "./pages/Home/Home";
+import Header from "./components/Layout/Header/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import About from "./pages/About/About";
 
 const App = () => {
-  
   // fetch('http://localhost:3005/data')
   // .then((response) => response.json())
   // .then((json) => console.log(json));
 
   return (
     <>
-    <Header />
-      <Routes >
-        <Route 
-        path='/'
-        element = {
-          <Home />
-        }/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
       </Routes>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
