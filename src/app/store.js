c1b570fcf499d072d05e90aa4f6fa8332abbf6b3
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-12-13 15:52:57 +0200

    store data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import brandRdeucer from "../features/brand/brandSlice";
+import categoriesRdeucer from "../features/categories/categoriesSlice";
+import bannerRdeucer from "../features/banner/bannerTop";
+import popularRdeucer from "../features/popular/popularBottom";
 
 export const store = configureStore({
   reducer: {
     brand: brandRdeucer,
+    categories: categoriesRdeucer,
+    banner: bannerRdeucer,
+    popular: popularRdeucer,
   },
 });
